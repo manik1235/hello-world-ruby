958dedd53b9b36127c668b3eb23a5bfcebd33d5e
Andrew Henning-Kolberg <Andrew.Kolberg@gmail.com>
2018-05-21 17:45:20 -0400

    first commit, running thru the tutorial

---
+++ hello-world.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+# My first foray into Ruby development!
+# Following the guide at https://ruby.github.io/TryRuby/
+puts 2 + 6
+puts "Andrew"
+puts "Andrew".reverse
+puts "Andrew".length
+puts "Andrew" * 5
+
+# Fails because it's not a method for a number
+40.reverse
